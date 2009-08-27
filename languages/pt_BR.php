1bb7d52f87d31909376be68f2d4fed96b8cadfaa
satollo <satollo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-27 12:57:35 +0000

    git-svn-id: https://plugins.svn.wordpress.org/newsletter/trunk@149593 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ languages/pt_BR.php
@@ -0,0 +1,35 @@
+<?php
+
+// Subscription form (traslate "your name", "your email" and the button "subscribe me")
+$newsletter_labels['subscription_form'] =
+'<form method="post" action="" style="text-align: center">
+<input type="hidden" name="na" value="s"/>
+<table cellspacing="3" cellpadding="3" border="0" width="50%">
+<tr><td>Seu&nbsp;nome</td><td><input type="text" name="nn" size="30"/></td></tr>
+<tr><td>Seu&nbsp;email</td><td><input type="text" name="ne" size="30"/></td></tr>
+<tr><td colspan="2" style="text-align: center"><input type="submit" value="Assinar"/></td></tr>
+</table>
+</form>';
+
+$newsletter_labels['widget_form'] =
+'<form action="{newsletter_url}" method="post">
+{text}
+<p><input type="text" name="nn" value="Seu nome" onclick="if (this.defaultValue==this.value) this.value=\'\'" onblur="if (this.value==\'\') this.value=this.defaultValue"/></p>
+<p><input type="text" name="ne" value="Seu email" onclick="if (this.defaultValue==this.value) this.value=\'\'" onblur="if (this.value==\'\') this.value=this.defaultValue"/></p>
+<p><input type="submit" value="Assinar"/></p>
+<input type="hidden" name="na" value="s"/>
+</form>';
+
+$newsletter_labels['embedded_form'] =
+'<form action="{newsletter_url}" method="post">
+<p><input type="text" name="ne" value="Seu email" onclick="if (this.defaultValue==this.value) this.value=\'\'" onblur="if (this.value==\'\') this.value=this.defaultValue"/>
+&nbsp;<input type="text" name="nn" value="Seu nome" onclick="if (this.defaultValue==this.value) this.value=\'\'" onblur="if (this.value==\'\') this.value=this.defaultValue"/>
+<input type="submit" value="Assinar"/>
+<input type="hidden" name="na" value="s"/></p>
+</form>';
+
+// Errors on subscription
+$newsletter_labels['error_email'] = 'Endereço de email incorreto. <a href="javascript:history.back()">Go back</a>.';
+$newsletter_labels['error_name'] = 'O nome não pode estar vazio. <a href="javascript:history.back()">Go back</a>.';
+
+?>
