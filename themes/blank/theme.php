962f6ffb5da1df8ad25aca277a430b36c4049191
satollo <satollo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-12 21:16:34 +0000

    git-svn-id: https://plugins.svn.wordpress.org/newsletter/trunk@384206 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- themes/blank/theme.php
+++
@@ -1,1 +0,0 @@
-Blank theme, use your fantasy!
--- themes/messages/theme.php
+++
@@ -1,9 +0,0 @@
-<div style="background-color: #999; padding: 30px;">
-    <div style="background-color: #fff; border: 3px solid #666; padding: 20px;">
-
-        <div style="font-family: sans-serif; font-size: 24px; color: #999"><?php echo get_option('blogname'); ?></div>
-
-        {message}
-
-    </div>
-</div>
