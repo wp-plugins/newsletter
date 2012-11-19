a53b8caa05a4c8648b1730edd61d6238dac6b5f9
satollo <satollo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-19 15:32:40 +0000

    Version number fix
    git-svn-id: https://plugins.svn.wordpress.org/newsletter/tags/3.0.0@627085 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
   Plugin Name: Newsletter
   Plugin URI: http://www.satollo.net/plugins/newsletter
   Description: Newsletter is a cool plugin to create your own subscriber list, to send newsletters, to build your business. <strong>Before update give a look to <a href="http://www.satollo.net/plugins/newsletter#update">this page</a> to know what's changed.</strong>
-  Version: 3.0.0b6
+  Version: 3.0.0
   Author: Stefano Lissa
   Author URI: http://www.satollo.net
   Disclaimer: Use at your own risk. No warranty expressed or implied is provided.
